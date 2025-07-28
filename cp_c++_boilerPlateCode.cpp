bc49afdc9c9ca0874742c7fe239d13de8336aa2e
adityas-28 <the.adityasingh280105@gmail.com>
2025-07-28 23:14:22 +0530

    add cp c++ boiler plate code

---
+++ cp_c++_boilerPlateCode.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+const int MOD = 1e9 + 7;
+
+void solve()
+{
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
