678586bd195c6b59d24d7d8426dceed990aee788
adityas-28 <the.adityasingh280105@gmail.com>
2026-02-08 23:49:37 +0530

    refactor gcd implementation and update boilerplate code

--- cp_c++_boilerPlateCode.cpp
+++ cp_c++_boilerPlateCode.cpp
@@ -6,6 +6,7 @@ const int MOD = 1e9 + 7;
 
 void solve()
 {
+    
 }
 
 int main()
@@ -13,7 +14,7 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int t;
+    int t = 1;
     cin >> t;
     while (t--)
     {
--- gcd.cpp
+++ gcd.cpp
@@ -1,8 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int gcd(int a, int b){
-    if(b == 0)
+int gcd_recursive(int a, int b)
+{
+    if (b == 0)
         return a;
-    return gcd(b, a % b);
+    return gcd_recursive(b, a % b);
+}
+
+int gcd_iterative(int a, int b)
+{
+    while (b > 0)
+    {
+        int temp = a % b;
+        a = b;
+        b = temp;
+    }
+    return a;
+}
+
+int main()
+{
+    int a = 13214214;
+    int b = 641134;
+
+    int stlGCD = gcd(a, b);
+    int recGCD = gcd_recursive(a, b);
+    int iterGCD = gcd_iterative(a, b);
+
+    cout << stlGCD << " " << recGCD << " " << iterGCD << endl;
 }
